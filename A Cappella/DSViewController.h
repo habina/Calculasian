352f02e22b91a3d8fa7ab6b54e960791c6c01e3d
Dasheng Chen <habinacds@gmail.com>
2014-08-21 14:24:21 -0700

    Initial Commit

---
+++ A Cappella/DSViewController.h
@@ -0,0 +1,13 @@
+//
+//  DSViewController.h
+//  A Cappella
+//
+//  Created by Dasheng Chen on 8/21/14.
+//  Copyright (c) 2014 CDS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DSViewController : UIViewController
+
+@end
---
+++ A Cappella/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  A Cappella
+//
+//  Created by Dasheng Chen on 8/21/14.
+//  Copyright (c) 2014 CDS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DSAppDelegate class]));
+    }
+}
